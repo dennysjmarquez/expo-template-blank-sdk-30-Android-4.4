d32f06fb859c2612a5ffd6a9723c6576abb35608
Brent Vatne <brentvatne@gmail.com>
2016-08-02 21:38:58 +0000

    Get rid of a bunch of stuff everyone has to delete, move to @exponent/samples
    fbshipit-source-id: 4af7e21

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,49 +2,18 @@
  * @providesModule HomeScreen
  */
 
-import React, { PropTypes } from 'react';
+import React, {
+  PropTypes,
+} from 'react';
 import {
-  Image,
   ScrollView,
   StyleSheet,
-  Text,
-  View,
 } from 'react-native';
-import { Constants } from 'exponent';
+import {
+  ExponentConfigView,
+} from '@exponent/samples';
 
 import BrandedNavigationTitle from 'BrandedNavigationTitle';
-
-const SectionHeader = ({title}) => {
-  return (
-    <View style={styles.sectionHeaderContainer}>
-      <Text style={styles.sectionHeaderText}>
-        {title}
-      </Text>
-    </View>
-  );
-};
-
-const SectionContent = (props) => {
-  return (
-    <View style={styles.sectionContentContainer}>
-      {props.children}
-    </View>
-  );
-};
-
-const AppIconPreview = ({iconUrl}) => {
-  if (!iconUrl) {
-    iconUrl = 'https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png';
-  }
-
-  return (
-    <Image
-      source={{uri: iconUrl}}
-      style={{width: 64, height: 64}}
-      resizeMode="cover"
-    />
-  );
-};
 
 export default class HomeScreen extends React.Component {
   static route = {
@@ -53,172 +22,21 @@ export default class HomeScreen extends React.Component {
     },
   }
 
-  _renderTitle() {
-    const { manifest } = Constants;
-
-    return (
-      <View style={styles.titleContainer}>
-        <View style={styles.titleIconContainer}>
-          <AppIconPreview iconUrl={manifest.iconUrl} />
-        </View>
-
-        <View style={styles.titleTextContainer}>
-          <Text style={styles.nameText} numberOfLines={1}>
-            {manifest.name}
-          </Text>
-
-          <Text style={styles.slugText} numberOfLines={1}>
-            {manifest.slug}
-          </Text>
-
-          <Text style={styles.descriptionText}>
-            {manifest.description}
-          </Text>
-        </View>
-      </View>
-    );
-  }
-
   render() {
-    const { manifest } = Constants;
     const { route } = this.props;
 
     return (
       <ScrollView
         style={styles.container}
         contentContainerStyle={route.getContentContainerStyle()}>
-        {this._renderTitle()}
-
-        <SectionContent>
-          <Text style={styles.sectionContentText}>
-            {manifest.sdkVersion}
-          </Text>
-        </SectionContent>
-
-        <SectionHeader title="version" />
-        <SectionContent>
-          <Text style={styles.sectionContentText}>
-            {manifest.version}
-          </Text>
-        </SectionContent>
-
-        <SectionHeader title="orientation" />
-        <SectionContent>
-          <Text style={styles.sectionContentText}>
-            {manifest.orientation}
-          </Text>
-        </SectionContent>
-
-        <SectionHeader title="primaryColor" />
-        <SectionContent>
-          <Color value={manifest.primaryColor} />
-        </SectionContent>
-
-        <SectionHeader title="iconUrl" />
-        <SectionContent>
-          <Text style={styles.sectionContentText}>
-            {manifest.iconUrl}
-          </Text>
-        </SectionContent>
-
-        <SectionHeader title="loading: iconUrl" />
-        <SectionContent>
-          <Text style={styles.sectionContentText}>
-            {manifest.loading && manifest.loading.iconUrl}
-          </Text>
-        </SectionContent>
-
-        <SectionHeader title="notification: iconUrl" />
-        <SectionContent>
-          <Text style={styles.sectionContentText}>
-            {manifest.notification && manifest.notification.iconUrl}
-          </Text>
-        </SectionContent>
-
-        <SectionHeader title="notification: color" />
-        <SectionContent>
-          {manifest.notification && <Color value={manifest.notification.color} />}
-        </SectionContent>
+        <ExponentConfigView />
       </ScrollView>
     );
   }
 }
 
-const Color = ({value}) => {
-  if (!value) {
-    return <View />;
-  } else {
-    return (
-      <View style={styles.colorContainer}>
-        <View style={[styles.colorPreview, {backgroundColor: value}]} />
-        <View style={styles.colorTextContainer}>
-          <Text style={styles.sectionContentText}>
-            {value}
-          </Text>
-        </View>
-      </View>
-    );
-  }
-};
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  titleContainer: {
-    paddingHorizontal: 15,
-    paddingTop: 15,
-    paddingBottom: 15,
-    flexDirection: 'row',
-  },
-  titleIconContainer: {
-    marginRight: 15,
-    paddingTop: 2,
-  },
-  sectionHeaderContainer: {
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: '#ededed',
-  },
-  sectionHeaderText: {
-    fontSize: 14,
-  },
-  sectionContentContainer: {
-    paddingTop: 8,
-    paddingBottom: 12,
-    paddingHorizontal: 15,
-  },
-  sectionContentText: {
-    color: '#808080',
-    fontSize: 14,
-  },
-  nameText: {
-    fontWeight: '600',
-    fontSize: 20,
-  },
-  slugText: {
-    color: '#a39f9f',
-    fontSize: 14,
-    marginTop: -1,
-    backgroundColor: 'transparent',
-  },
-  descriptionText: {
-    fontSize: 14,
-    marginTop: 6,
-    color: '#4d4d4d',
-  },
-  colorContainer: {
-    flexDirection: 'row',
-  },
-  colorPreview: {
-    width: 17,
-    height: 17,
-    borderRadius: 2,
-    marginRight: 6,
-  },
-  colorTextContainer: {
     flex: 1,
   },
 });
