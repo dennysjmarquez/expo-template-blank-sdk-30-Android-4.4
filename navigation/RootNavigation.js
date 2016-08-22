7373fe67074c2427884b5d4d47e65edb21b71ec8
Brent Vatne <brentvatne@gmail.com>
2016-08-22 00:02:47 +0000

    Make it clear how people can stop receiving push notifs
    fbshipit-source-id: e730a85

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -69,6 +69,9 @@ export default class RootNavigation extends React.Component {
     const { notification } = this.props;
 
     // Send our push token over to our backend so we can receive notifications
+    // You can comment the following line out if you want to stop receiving
+    // a notification every time you open the app. Check out the source
+    // for this function in api/registerForPushNotificationsAsync.js
     registerForPushNotificationsAsync();
 
     // If we started the app from a push notification, handle it right away
