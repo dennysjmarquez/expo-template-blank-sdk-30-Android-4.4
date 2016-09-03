e66c59876bacc4f54c3bc0e3ebe706548136a381
Brent Vatne <brentvatne@gmail.com>
2016-09-03 17:54:56 +0000

    Show a console warning if caching assets fails, and continue
    fbshipit-source-id: ee88fc2

--- main.js
+++ main.js
@@ -31,17 +31,24 @@ class AppContainer extends React.Component {
   }
 
   async _loadAssetsAsync() {
-    await cacheAssetsAsync({
-      images: [
-        require('./assets/images/exponent-wordmark.png'),
-      ],
-      fonts: [
-        FontAwesome.font,
-        {'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')},
-      ],
-    });
-
-    this.setState({appIsReady: true});
+    try {
+      await cacheAssetsAsync({
+        images: [
+          require('./assets/images/exponent-wordmark.png'),
+        ],
+        fonts: [
+          FontAwesome.font,
+          {'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')},
+        ],
+      });
+    } catch(e) {
+      console.warn(`
+        There was an error caching assets (see: main.js), perhaps due to a
+        network timeout, so we skipped caching. Reload the app to try again.
+      `);
+    } finally {
+      this.setState({appIsReady: true});
+    }
   }
 
   render() {
