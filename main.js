3e8a4c9285e75c9892cb0be645dc5ef04868fcbe
Brent Vatne <brentvatne@gmail.com>
2016-09-10 03:04:35 +0000

    Update config pre-SDK10
    fbshipit-source-id: f3a8c9f

--- main.js
+++ main.js
@@ -37,15 +37,11 @@ class AppContainer extends React.Component {
           require('./assets/images/exponent-wordmark.png'),
         ],
         fonts: [
-          FontAwesome.font,
           {'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')},
         ],
       });
     } catch(e) {
-      console.warn(`
-        There was an error caching assets (see: main.js), perhaps due to a
-        network timeout, so we skipped caching. Reload the app to try again.
-      `);
+      console.warn(`There was an error caching assets (see: main.js), perhaps due to a network timeout, so we skipped caching. Reload the app to try again.`);
     } finally {
       this.setState({appIsReady: true});
     }
