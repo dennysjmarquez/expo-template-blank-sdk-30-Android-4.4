dadecf341ae07e9d89a17f85ba8436f528d47a67
Brent Vatne <brentvatne@gmail.com>
2017-09-01 06:55:59 +0000

    Improve error handling & reporting in development (#747)
    * [expo-sdk] Symbolicate unhandled promise rejection so logs are useful and easy to read
    
    * [expo-sdk][new-project-template] Change AppLoading to new style that lets us catch errors
    
    * [expo-sdk] Handle error that occurs during initial render, rather than leaving loading screen open
    
    * [expo-sdk] Clean up error message screens
    
    * Clean up and improve logging (any error that is logged gets symbolicated!)
    
    * Handle console.error better, remove max stack depth
    
    * Refactor error logging
    
    * Add method names to trace, improve error screen
    
    * Add stacks to console.warn and console.error
    
    * Include projectRoot in manifest, handle stack trace logs in exp
    
    * Newlines make exp easier to read
    - I removed them at one point, I was wrong.
    
    * Handle stack traces in XDE -- collapsable
    
    * Auto expand console.error, use * instead of - for app code, like in exp
    
    * Add a little bit of bottom margin to normal logs
    
    * Make file paths in stack trace clickable
    
    * Fix case where warning isn't a string
    
    * Handle logging error objects properly
    
    * Update AppLoading docs
    
    * Only use global error handler when AppLoading is mounted
    
    * Fix font style on error screen
    
    * Only called ExponentAppLoadingManager.finishedAsync on error if AppLoading is mounted
    
    * Refactor logging
    
    * Add comment to clarify _stackTraceLogsSupported and make it check for dev
    
    * Clean up exp log functions a bit
    
    * Remove newlines before logs
    
    * Pad trace logs with newlines -- to do this, we need to track whether previous line is newline
    
    * Pad stack trace logs with a line before and after
    
    * Address @ide's feedback
    
    * Fix flow errors
    
    * More feedback from @ide
    
    * More more feedback from @ide
    
    * Add console.warn to default loading error handler on new template
    
    * Address flow feedback
    
    fbshipit-source-id: 5fb158d

--- App.js
+++ App.js
@@ -6,16 +6,18 @@ import RootNavigation from './navigation/RootNavigation';
 
 export default class App extends React.Component {
   state = {
-    assetsAreLoaded: false,
+    isLoadingComplete: false,
   };
 
-  componentWillMount() {
-    this._loadAssetsAsync();
-  }
-
   render() {
-    if (!this.state.assetsAreLoaded && !this.props.skipLoadingScreen) {
-      return <AppLoading />;
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
     } else {
       return (
         <View style={styles.container}>
@@ -28,33 +30,31 @@ export default class App extends React.Component {
     }
   }
 
-  async _loadAssetsAsync() {
-    try {
-      await Promise.all([
-        Asset.loadAsync([
-          require('./assets/images/robot-dev.png'),
-          require('./assets/images/robot-prod.png'),
-        ]),
-        Font.loadAsync([
-          // This is the font that we are using for our tab bar
-          Ionicons.font,
-          // We include SpaceMono because we use it in HomeScreen.js. Feel free
-          // to remove this if you are not using it in your app
-          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
-        ]),
-      ]);
-    } catch (e) {
-      // In this case, you might want to report the error to your error
-      // reporting service, for example Sentry
-      console.warn(
-        'There was an error caching assets (see: App.js), perhaps due to a ' +
-          'network timeout, so we skipped caching. Reload the app to try again.'
-      );
-      console.log(e);
-    } finally {
-      this.setState({ assetsAreLoaded: true });
-    }
-  }
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync([
+        // This is the font that we are using for our tab bar
+        Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+      ]),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
 
 const styles = StyleSheet.create({
