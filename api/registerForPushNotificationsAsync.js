7b25286f20f3d18eb8aef624250022a25495a901
Charlie Cheever <ccheever@gmail.com>
2017-10-31 20:08:18 +0000

    Fixing push notification server endpoint (#1352)
    We accidentally left a test server in instead of the production one
    This fixes that.
    
    fbshipit-source-id: 065d2c0

--- api/registerForPushNotificationsAsync.js
+++ api/registerForPushNotificationsAsync.js
@@ -1,8 +1,7 @@
 import { Permissions, Notifications } from 'expo';
 
 // Example server, implemented in Rails: https://git.io/vKHKv
-// const PUSH_ENDPOINT = 'https://expo-push-server.herokuapp.com/tokens';
-const PUSH_ENDPOINT = 'http://5acf2105.ngrok.io';
+const PUSH_ENDPOINT = 'https://expo-push-server.herokuapp.com/tokens';
 
 export default (async function registerForPushNotificationsAsync() {
   // Android remote notification permissions are granted during the app
