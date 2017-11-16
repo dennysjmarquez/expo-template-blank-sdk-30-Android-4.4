56463287aa548413704d1aaf7b239b34d36e7591
Brent Vatne <brentvatne@gmail.com>
2017-11-16 23:38:33 +0000

    Update for sdk 23
    fbshipit-source-id: 3fa6c3e

--- App.js
+++ App.js
@@ -35,13 +35,13 @@ export default class App extends React.Component {
         require('./assets/images/robot-dev.png'),
         require('./assets/images/robot-prod.png'),
       ]),
-      Font.loadAsync([
+      Font.loadAsync({
         // This is the font that we are using for our tab bar
-        Ionicons.font,
+        ...Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
-      ]),
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
     ]);
   };
 
