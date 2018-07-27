cb97a05ea4c8f9e80ce4cd4b27882b67977fb956
Brent Vatne <brentvatne@gmail.com>
2018-07-27 01:34:38 +0000

    Update to 29.0.0
    fbshipit-source-id: 6fed013

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,21 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 import renderer from 'react-test-renderer';
+import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
 
-it('renders the loading screen', async () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('App snapshot', () => {
+  jest.useFakeTimers();
+  beforeEach(() => {
+    NavigationTestUtils.resetInternalState();
+  });
 
-it('renders the root without loading screen', async () => {
-  const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-  expect(tree).toMatchSnapshot();
+  it('renders the loading screen', async () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the root without loading screen', async () => {
+    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
